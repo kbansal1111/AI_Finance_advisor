f42d539ea209143abdb2d9e292ef06527568b733
kartik bansal <142658657+kbansal1111@users.noreply.github.com>
2024-11-16 14:03:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,13 @@ user_name=st.text_input('enter your name')
 st.write()
 monthly_income=st.number_input('your monthly income')
 st.write()
+product_category = st.selectbox(
+    "Select the Category",
+    options=["Food", "Travel", "Entertainment", "Bills", "Others"]
+)
+product_amount = st.number_input("Enter the Amount", min_value=0.0, step=0.01)
+if st.button("Add Expenditure"):
+    if amount > 0:
+        st.success(f"amount spend ₹ {product_amount} on {product_category}")
+    else:
+        st.error("Please enter a valid amount.")
