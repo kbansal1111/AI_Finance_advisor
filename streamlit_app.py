4626b39a02bdd5844d6678a41bb1d8ad4c9abb90
kartik bansal <142658657+kbansal1111@users.noreply.github.com>
2024-11-16 13:06:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.title('AI Expense Tracker')
-st.info('This AI model will help in your finance')
+st.info('This AI model will help u your finance')
+user_name=st.text_input('enter your name')
+st.write('user_name')
+monthly_income=st.number_input('your monthly income')
+st.wrtie(monthly_income)
