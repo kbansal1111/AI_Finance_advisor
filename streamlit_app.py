2f79a1af0563027d4e77be7b3825262763bd60d9
kartik bansal <142658657+kbansal1111@users.noreply.github.com>
2024-11-16 15:45:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+API_Key="AIzaSyCFZy1n4bvWj3QXAC1nrwJr5aPQl43H2_k"
+# App interface
 st.title('AI Expense Tracker')
 st.info('This AI model will help u your finance')
 user_name=st.text_input('enter your name')
@@ -31,8 +33,11 @@ if not st.session_state["expense"].empty:
 total_amount_spend=st.session_state["expense"]["product amount"].sum()
 st.write(f"Total expenditure is {total_amount_spend}")
 
+# Graph
 fig,ax = plt.subplots(figsize=(3,3))
 ax.bar(['monthly income', 'Total Expenditure'], [monthly_income, total_amount_spend], color=['green', 'red'])
 ax.set_ylabel('Amount')
 ax.set_title('Monthly Income vs Total Expenditure')
 st.pyplot(fig)
+
+# model using gemini api
